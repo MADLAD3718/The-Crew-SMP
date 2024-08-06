8f1c46ce96c41549dfc4b00e758ca88eed76fe60
MADLAD3718 <scheuringjack@gmail.com>
2024-08-06 15:52:15 -0400

    Finalise cannon firing mechanics

--- behaviours/scripts/entities/cannon.js
+++ behaviours/scripts/entities/cannon.js
@@ -1,21 +1,92 @@
-import { EntityComponentTypes, GameMode, ItemStack, world } from "@minecraft/server";
+import { Container, Entity, EntityComponentTypes, GameMode, ItemStack, Player, system, TicksPerSecond, world } from "@minecraft/server";
+import { add, Directions, length, mul } from "../extensions/vectors";
+
+const CANNON_DELAY = 1.0 * TicksPerSecond;
 
 world.afterEvents.entityHitEntity.subscribe(event => {
     const {hitEntity: cannon, damagingEntity} = event;
-    if (cannon.typeId !== "tcsmp:cannon") return;
+    if (cannon.typeId !== "tcsmp:cannon" || !(damagingEntity instanceof Player)) return;
 
     const rider = cannon.getComponent(EntityComponentTypes.Rideable).getRiders()[0];
     if (rider?.id === damagingEntity.id) {
-        console.warn(`Fire!`);
+        const time = rider.getDynamicProperty("cannon_fire_time") ?? 0;
+        if (system.currentTick - time < CANNON_DELAY) return;
+        const gunpowder = findItem("minecraft:gunpowder", cannon.inventory.container);
+        const cannonball = findAmmo(cannon.inventory.container);
+        if (gunpowder !== undefined && cannonball !== undefined) {
+            const ball_item = cannon.inventory.container.getItem(cannonball);
+            decrementSlot(cannon.inventory.container, gunpowder);
+            decrementSlot(cannon.inventory.container, cannonball);
+            
+            const view = rider.getViewDirection(), origin = add(cannon.location, Directions.Up);
+            const direction = {x: view.x, y: Math.max(view.y, 0), z: view.z};
+            const velocity = mul(direction, 3 / length(direction));
+
+            const ball = cannon.dimension.spawnEntity(ball_item.typeId, add(velocity, origin));
+            ball.getComponent(EntityComponentTypes.Projectile).shoot(velocity);
+
+            rider.setDynamicProperty("cannon_fire_time", system.currentTick);
+        }
     } else {
         if (damagingEntity?.getGameMode() !== GameMode.creative)
             cannon.dimension.spawnItem(new ItemStack("tcsmp:cannon"), cannon.location);
+        dropInventory(cannon);
         cannon.remove();
     }
 });
 
-world.afterEvents.dataDrivenEntityTrigger.subscribe(event => {
-    if (event.eventId !== "tcsmp:control_cannon") return;
-    const {entity: cannon} = event;
-    const player = cannon.getComponent(EntityComponentTypes.Rideable).getRiders()[0];
-});
+/**
+ * Finds the first instance of an item in a container.
+ * @param {String} itemId
+ * @param {Container} container 
+ * @returns {Number | undefined}
+ */
+function findItem(itemId, container) {
+    for (let i = 0; i < container.size; ++i) {
+        const item = container.getItem(i);
+        if (item?.typeId === itemId) return i;
+    }
+    return undefined;
+}
+
+/**
+ * Finds the first instance of a cannon ammo item in a container.
+ * @param {Container} container 
+ * @returns {Number | undefined}
+ */
+function findAmmo(container) {
+    for (let i = 0; i < container.size; ++i) {
+        const item = container.getItem(i);
+        if (item?.getTags().includes("cannon_ammo")) return i;
+    }
+    return undefined;
+}
+
+/**
+ * Decrements the itemstack count in the provided slot.
+ * @param {Container} container 
+ * @param {Number} slot 
+ */
+function decrementSlot(container, slot) {
+    if (slot >= container.size) return;
+    const item = container.getItem(slot);
+    if (item.amount == 1) {
+        container.setItem(slot);
+    } else {
+        item.amount--;
+        container.setItem(slot, item);
+    }
+}
+
+/**
+ * Drops the inventory of the entity.
+ * @param {Entity} entity 
+ */
+function dropInventory(entity) {
+    const container = entity.inventory.container;
+    for (let i = 0; i < container.size; ++i) {
+        const item = container.getItem(i);
+        if (!item) continue;
+        entity.dimension.spawnItem(item, entity.location);
+    }
+}
