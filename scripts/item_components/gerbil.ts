e949878735803f9b324c90182e61f116633ceb1f
MADLAD3718 <scheuringjack@gmail.com>
2025-04-02 16:15:08 -0400

    Tomato In Progress

--- scripts/item_components/gerbil.ts
+++ scripts/item_components/gerbil.ts
@@ -6,40 +6,29 @@ const PICKUP_TIMES: Map<string, number> = new Map();
 const PICKUP_DELAY = 0.25 * TicksPerSecond;
 
 world.afterEvents.playerInteractWithEntity.subscribe(event => {
-    const { player, target, itemStack } = event;
+    const { player, target } = event;
     if (!target.matches({type: "tcsmp:gerbil"})) return;
-    if (!player.isSneaking) return;
 
-    if (!itemStack) {
-        if (!target.nameTag) return;
+    const tameable = target.getComponent(EntityComponentTypes.Tameable);
+    if (tameable?.tamedToPlayerId != player.id) return;
 
-        const tameable = target.getComponent(EntityComponentTypes.Tameable);
-        if (tameable?.isTamed && tameable.tamedToPlayerId != player.id) return;
+    const item = new ItemStack("tcsmp:gerbil");
+    item.nameTag = target.nameTag;
     
-        const item = new ItemStack("tcsmp:gerbil");
-        item.nameTag = target.nameTag;
-        
-        target.remove();
-        player.equipment.getEquipmentSlot(EquipmentSlot.Mainhand).setItem(item);
-    
-        const head = player.getHeadLocation();
-        player.dimension.playSound("random.pop", head);
-        player.dimension.playSound("mob.gerbil.chirp", head);
-    
-        PICKUP_TIMES.set(player.id, system.currentTick);
-    }
-    else if (itemStack.typeId == MinecraftItemTypes.NameTag) {
-        if (target.nameTag) return;
+    target.remove();
+    player.equipment.getEquipmentSlot(EquipmentSlot.Mainhand).setItem(item);
 
-        const tameable = target.getComponent(EntityComponentTypes.Tameable);
-        if (!tameable?.isTamed) tameable?.tame(player);
-    }
+    const head = player.getHeadLocation();
+    player.dimension.playSound("random.pop", head);
+    player.dimension.playSound("mob.gerbil.chirp", head);
+
+    PICKUP_TIMES.set(player.id, system.currentTick);
 });
 
 const gerbilComponent: ItemCustomComponent = {
     onUseOn(event) {
         const { source, itemStack } = event;
-        if (!(source instanceof Player && source.isSneaking)) return;
+        if (!(source instanceof Player)) return;
 
         const lastPickedUpTime = PICKUP_TIMES.get(source.id) ?? 0;
         if (system.currentTick - lastPickedUpTime < PICKUP_DELAY) return;
